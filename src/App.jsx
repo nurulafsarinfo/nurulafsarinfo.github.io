133194c91ccc3e0cb790c10fd6340d5e98457e87
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-06-28 15:49:57 +0600

    Navbar updated with new profile logo and style tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 
 const App = () => {
   return (
     <div className=''>
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
-// src/components/Navbar.jsx
+import React from "react";
+import logo from "../assets/myProfile-Logo.png";
 const Navbar = () => {
 
     const navLinks = (
@@ -15,7 +16,10 @@ const Navbar = () => {
     return (
         <div className="navbar bg-[#060b1d] text-white fixed top-0 z-50 shadow-md px-8">
             <div className="navbar-start">
-                <a href="#home" className="text-2xl">Nurul.dev</a>
+                <a href="#home">
+                    <img src={logo} alt="Profile logo" className="w-12 h-12 rounded-full " />
+                </a>
+                {/* <a href="#home" className="text-2xl">Nurul.dev</a> */}
             </div>
 
 
@@ -30,28 +34,27 @@ const Navbar = () => {
                 <a href="/Afsar_Resume.pdf"
                     download
                     className="btn btn-sm bg-[#1348e7] text-white border-none hover:bg-blue-800 transition duration-300">Download Resume</a>
+
+                {/* Form Mobile Dropdown  */}
+                <div className="dropdown lg:hidden relative">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6 text-white"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </label>
+
+                    <ul tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-3 shadow bg-[#060b1d] rounded-lg w-30 space-y-2 absolute right-2">
+                        {navLinks}
+                    </ul>
+                </div>
             </div>
-
-            {/* Form Mobile Dropdown  */}
-            <div className="dropdown lg:hidden">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 text-white"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                            d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                </label>
-
-                <ul tabIndex={0}
-                    className="menu">
-                    {navLinks}
-                </ul>
-            </div>
-
         </div>
     );
 };
